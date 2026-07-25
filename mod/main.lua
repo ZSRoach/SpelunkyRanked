e7bff34c84b0ae33f1b0a343a6e613326d438c1e
ZSRoach <zachary@okmaybe.com>
2026-07-25 15:28:12 -0400

    version bump on mod

--- mod/main.lua
+++ mod/main.lua
@@ -1,7 +1,7 @@
 meta = {
     name = 'S2 Ranked',
     version = '1.21',
-    component_version = '1.21.2',
+    component_version = '1.21.3',
     description = '1v1 Spelunky For Rank',
     author = 'ZSRoach',
     unsafe = true,
